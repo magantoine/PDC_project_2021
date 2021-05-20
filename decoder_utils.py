72ef9a57574446475f0171c82f2a9a1a0ba32e53
Noureddine Gueddach <noureddach@gmail.com>
2021-05-20 17:29:06 +0200

    Implement Viterbi

--- decoder_utils.py
+++ decoder_utils.py
@@ -2,45 +2,159 @@ import numpy as np
 
 class Decoder:
 
-    def __init__(self, input_file, output_file, n, verbose=False):
+    def __init__(self, input_file, output_file, generator_matrix, verbose=False):
         self.input_file = input_file
         self.output_file = output_file
         self.verbose = verbose
-        self.n = n
+        # The generator matrix holds the generator polynomials used to select which of the b_i participate in the product
+        self.generator_matrix = generator_matrix
+        self.N = generator_matrix.shape[0] # N is the number of output bits per input bit
+        self.L = generator_matrix.shape[1] # L is the number of previous bits used when encoding (b_{i-L} ... b_{i-1})
     
-    def euclidean_distance(self, x, y):
-        x = np.array(x)
-        y = np.array(y)
-        p = np.sum((x - y)**2)
-        return np.sqrt(p)
-    
-    def bits_to_bytes(self, bits_array):
-        done = False
-        if len(bits_array) % 8 != 0:
-            print('Corrupted bits array. Length should be multiple of 8')
-        bytes_array = []
-        it = 0
-        for i in range(len(bits_array)//8):
-            byte = 0
-            for _ in range(8):
-                bit = bits_array[it]
-                byte = (byte << 1) | bit
-                it += 1
-            bytes_array.append(byte)
-        return bytes_array
-    
-    # Reads array of floats and decodes them into integers (from the Alphabet)
-    def decode_values(self, values):
-        decoded_values = []
+    def guess_J(self, values):
+        """
+        Guess the J index by taking the sum of squares of all indices = 0, 1, 2 mod 3 respectively and setting J = argmin
+        Probability of error is proportional to the total number bits and thus extremely low
+        """
         # Guess the erased index
         J = np.argmin(np.array([np.sum(values[::3]**2), np.sum(values[1::3]**2), np.sum(values[2::3]**2)]))
+        if self.verbose:
+            print(f'Erased index is J = {J}')
+        return J
+    
+    def compactify_values(self, values, J):
+        """
+        Removes the values at index J and output the mean of the remaining 2 values
+        Note that instead of throwing one of the bits away, taking the mean makes sure that the output value has only
+        half the original noise variance!
+        """
+        compact_values = []
+        values = np.delete(values, np.arange(J, values.size, 3)) # Delete 1 value out of 3 at multiples of the J index
+        for x,y in zip(*[iter(values)]*2):
+            compact_values.append((x+y)/2.) # Take the mean of the remaining 2 values (this reduces variance)
+        return compact_values
+    
+    def generate_dico(self, n):
+        """
+        Generates the dictionary that represents the state diagram
+        Given a key K = {bit_sequence} = {b_{i-L} ... {b_i}} it returns the output X of the encoding
+        These values or computed only once for efficiency
+        """
+        dico = {}
+
+        def compute_output(vals):
+            encoded_vals = []
+            for generator_poly in self.generator_matrix: # We output 1 bit per generator polynomial
+                elems = generator_poly * list(vals) # Select only part of the bits
+                encoded_val = np.prod(elems[elems != 0]) # Remove the non-selected bits and take the product
+                                                         # Corresponds to b_{i-L}b_{i-3}b_i for example
+                encoded_vals.append(encoded_val)
+            return encoded_vals
+
+        def generateAllBinaryStrings(n, arr, i): 
+            if i == n:
+                string = list_to_string(arr)
+                dico[string] = compute_output(arr)
+                return
+            arr[i] = -1
+            generateAllBinaryStrings(n, arr, i + 1) 
+            arr[i] = 1
+            generateAllBinaryStrings(n, arr, i + 1)
+
+        arr = [None] * n
+        generateAllBinaryStrings(n, arr, 0)
+        return dico
+    
+    def viterbi_decoding(self, values):
+        """
+        Classical Viterbi decoding algorithm
+        A key difference is that we don't pad with a final ending sequence of 1's when we finish, 
+        we simply look for the state with the highest metric on the last depth with we finish processing all Y's.
+        """
+        dico = self.generate_dico(self.L) # Compute the output values of the state diagram
+        if len(values) % self.N != 0:
+            print(f'Corrupted values of length {len(values)}. Should be a multiple of {self.N}')
+        nb_trellis_sections = int(len(values)/self.N)
+        print(f'nb_trellis_sections : {nb_trellis_sections}')
         
-        values = np.delete(values, np.arange(J, values.size, 3))
+        # Viterbi trellis dimensions :
+        height = 2**(self.L-1)
+        width = nb_trellis_sections + 1 # + 1 to account for the very first state
+        print(f'Trellis height : {height}')
+        print(f'Trellis width  : {width}')
         
-        for x in zip(*[iter(values)]*self.n):
-            d1 = self.euclidean_distance(list(x), np.full(shape=self.n, fill_value=1, dtype=np.int))
-            d2 = self.euclidean_distance(list(x), np.full(shape=self.n, fill_value=-1, dtype=np.int))
-            decoded_values.append(1 if d1 <= d2 else 0)
+        #Create the trellis with different arrays that stores various informations
+        trellis_metrics = np.full((height, width), fill_value=float('-inf'), dtype=float) # Stores the metrics
+        trellis_backpointers = np.full((height, width), fill_value="", dtype=object) # Stores the backpointers (used for backtracking)
+        trellis_reaching_bits = np.ndarray((height, width), dtype=int) # Stores the bit used to reach this state
+
+        idx = 1
+        first_entry = '1' * (self.L - 1) # We start at state '11...1'
+        first_entry_int = plus_minus_string_to_int(first_entry)
+        trellis_metrics[first_entry_int, 0] = 0 # Initialize the trellis metric to 0
+        active_entries = [first_entry] # Stores the states that have been reached in the previous iteration
+        
+        print('Decoding ...')
+        
+        for Y in zip(*[iter(values)]*self.N): # decompose into chunks of self.N lengths to process each trellis section independently
+            new_active_entries = [] # We will set the states we reach as active so that they can be used for the next iteration
+            for current_state in active_entries:
+                for i in [1, -1]: # Handle both arrows (+1 and -1)
+                    key = current_state + str(i) # represents {b_{i-L} ... b_i}
+                    X = dico[key] # returns a list of self.N ints (the X-vector in the Viterbi algorithm)
+                    metric = np.sum(np.array(list(Y)) * X) # Compute the metric
+                    next_state = list_to_string(string_to_list(key)[1:]) # We get the next state by forgetting b_{i-L}
+
+                    # Simply transforms the states to ints so that they can be used as indexes in the numpy arrays
+                    current_state_int = plus_minus_string_to_int(current_state) 
+                    next_state_int = plus_minus_string_to_int(next_state)
+
+                    curr_best_metric = trellis_metrics[next_state_int, idx] # Current metric of the next state
+                    potential_metric = metric + trellis_metrics[current_state_int, idx-1] # Metric if we reach it from current state
+                    if potential_metric > curr_best_metric: #If higher metric, we update the metric, back_pointers etc...
+                        trellis_metrics[next_state_int, idx] = potential_metric
+                        trellis_backpointers[next_state_int, idx] = current_state
+                        trellis_reaching_bits[next_state_int, idx] = i
+                        new_active_entries.append(next_state)
+            active_entries = new_active_entries
+            idx += 1
+            print("Progress: " + "{:.2f}".format(100*idx/nb_trellis_sections)+ "%", end='\r')
+        
+        idx -= 1
+        # Find endpoint
+        # We look for the state with highest metric on the final depth
+        # Note that we don't need to add dummy bits to get to the state '11...1', it is enough to find the state with highest metric
+        final_entry = ""
+        best_metric = float('-inf')
+        for active_entry in active_entries:
+            active_entry_int = plus_minus_string_to_int(active_entry) # Transform state to int to be used as index
+            metric = trellis_metrics[active_entry_int, idx]
+            if metric > best_metric:
+                best_metric = metric
+                final_entry = active_entry
+                            
+        # Backtrack
+        # Once we have the 
+        decoded_values = []
+        while idx > 0:
+            final_entry_int = plus_minus_string_to_int(final_entry) # Transform state to int to be used as index
+            decoded_val = trellis_reaching_bits[final_entry_int, idx] # The decoded value is the bit we used to reach this state
+            decoded_values.append((0 if decoded_val == -1 else 1))
+            final_entry = trellis_backpointers[final_entry_int, idx] # We go to the state on previous depth
+            idx -= 1
+            
+        return list(reversed(decoded_values)) # Need to reverse since we traversed the trellis backwards
+    
+    # Reads array of floats and decodes them into bits
+    def decode_values(self, values):
+        decoded_values = []
+        J = self.guess_J(values)
+        values = self.compactify_values(values, J) # get rid of the deleted bits and merge the 2 remaining ones
+        
+        #At this stage we got rid of the repetition code
+        # We now need to decode using Viterbi
+        decoded_values = self.viterbi_decoding(values)
+
         if self.verbose:
             print(f'Decoded values : {decoded_values}\n')
         return decoded_values
@@ -48,15 +162,73 @@ class Decoder:
     def decode(self):
         # Read raw values from file
         values_array = np.loadtxt(self.input_file)
+        if self.verbose:
+            print(f'Nb values to decode : {len(values_array)}')
         # Decode the values
         decoded_values = self.decode_values(values_array)
         # Transform the bit array to bytes
-        byte_array = bytearray(self.bits_to_bytes(decoded_values))
+        byte_array = bits_to_bytes(decoded_values)
         
-        # if self.verbose:
         print(byte_array)
         f = open(self.output_file, 'w+b')
         f.write(byte_array)
         f.close()
-    
-   
+
+def bits_to_bytes(bits_array):
+    """
+    Group 8-bits into bytes and return the corresponding byte array
+    """
+    done = False
+    if len(bits_array) % 8 != 0:
+        print(f'Corrupted bits array. Length should be multiple of 8, but was {len(bits_array)}')
+    bytes_array = []
+    it = 0
+    for i in range(len(bits_array)//8):
+        byte = 0
+        for _ in range(8):
+            bit = bits_array[it]
+            byte = (byte << 1) | bit
+            it += 1
+        bytes_array.append(byte)
+    return bytearray(bytes_array)   
+
+
+def string_to_list(string):
+    """
+    Transform a string of {+1, -1} into a list of {+1, -1}
+    Example : '1-1-11' is transformed into [1, -1, -1, 1]
+    """
+    list = []
+    i = 0
+    while i < len(string):
+        if string[i] == '1':
+            list.append(1)
+            i += 1
+        else:
+            list.append(-1)
+            i += 2
+    return np.array(list)
+
+def list_to_string(list):
+    """
+    Transform a list of {+1, -1} into a string of {+1, -1}
+    Example : [1, -1, -1, 1] is transformed into '1-1-11'
+    """
+    s = ""
+    for i in list:
+        s += str(i)
+    return s
+
+def plus_minus_string_to_int(string):
+    """
+    Transform a string of {+1, -1} into an integer, where -1 is interpreted as 0 and 1 is interpreted as 1
+    Example : '1-1-11' is equivalent to '1001' and is transformed to 1 + 2 * 0 + 4 * 0 + 8 * 1 = 9
+    """
+    li = list(string_to_list(string))
+    li = list(map(lambda e: (0 if e == -1 else 1), li))
+    power = 1
+    number = 0
+    for i in li:
+        number += i * power
+        power *= 2
+    return number
--- encoder_utils.py
+++ encoder_utils.py
@@ -1,81 +1,90 @@
 import numpy as np
+from collections import deque
 
 class Encoder:
 
-    def __init__(self, input_file, output_file, n, verbose=False):
+    def __init__(self, input_file, output_file, generator_matrix, verbose=False):
         self.input_file = input_file
         self.output_file = output_file
         self.verbose = verbose
-        self.n = n // 2 * 3 # Account for the erased bit
-        
-    def file_to_bits(self, filename, little_endian=True):
-        bits = []
-        with open(filename, 'rb') as f:
-            bytes = f.read()
-            if self.verbose:
-                print(f'Original file : {bytes}\n')
-            for b in bytes:
-                for i in range(8):
-                    bits.append((b >> i) & 1 if little_endian else (b >> (7 - i)) & 1)
-            if self.verbose:
-                print(f'Bits array : {bits}\n')
-            return np.array(bits)
+        # The generator matrix holds the generator polynomials used to select which of the b_i participate in the product
+        self.generator_matrix = generator_matrix
+        self.N = generator_matrix.shape[0] # N is the number of output bits per input bit
+        self.L = generator_matrix.shape[1] # L is the number of previous bits used when encoding (b_{i-L} ... b_{i-1})
     
-    # Encode bits into values in [-1, 1]
-    def bits_to_values(self, bits_array):
-        values = []
-        for bit in bits_array:
-            values.extend(np.full(shape=self.n, fill_value=(-1 if bit == 0 else 1), dtype=np.int))
+    def repetition_encoding(self, array, nb_repetitions=3):
+        """
+        Apply a repetition code to the input array
+        """
+        repeated_array = []
+        for val in array:
+            repeated_array.extend(np.full(shape=nb_repetitions, fill_value=val, dtype=np.int))
         if self.verbose:
-            print(f'Encoded values : {values}\n')
-        return values
-
-    def convolution(self, bits):
+            print(f'Repeated values : {repeated_array}\n')
+        return repeated_array
+    
+    def convolutional_encoding(self, array):
         """
-        does the convolution of the bits to obtain the output the following fashion:
-
-        b_n => (x_{3n}, x_{3n+1}, x_{3n+2}) = (b_n*b_{n-2}, b_{n-1}*b_{n-2}, b_n*b_{n-1}*b_{n-2})
+        Apply convolutional coding on the input array, using the encoder's generator matrix
         """
-        xs = np.zeros((2*len(bits),))
-        for i in range(len(bits)):
-            if(i == 0):
-                xs[2*i] = bits[i]
-                xs[2*i + 1] = bits[i]
-                #xs[2*i + 2] = bits[i]
-            if(i == 1):
-                xs[2*i] = bits[i]
-                xs[2*i + 1] = bits[i - 1] * bits[i]
-                #xs[2*i + 2] = bits[i - 1] * bits[i]
-            else :
-                xs[2*i] = bits[i] * bits[i - 2]
-                xs[2*i + 1] = bits[i - 1] * bits[i - 2] * bits[i]
-                #xs[3*i + 2] = bits[i] * bits[i - 1] * bits[i - 2]
-
-        return xs
+        encoded_vals = []
+        padded_array = [1] * (self.L - 1) + array # pre-pad the array with (L-1) ones (b_{i-L}...b_{i-1} are initialized to 1)
+        for vals in window(padded_array, size=self.L):
+            for generator_poly in self.generator_matrix: # For each generator polynomial we output one bit
+                elems = generator_poly * list(vals) # Select the values that will participate in the product
+                encoded_val = np.prod(elems[elems != 0]) # Discard the 0 values and perform the product
+                encoded_vals.append(encoded_val)
+        if self.verbose:
+            print(f'Convolved values : {encoded_vals}\n')
+        return encoded_vals
     
     def encode(self):
-        bits_array = self.file_to_bits(self.input_file, little_endian=False)
+        """
+        Encode the input sequence for sending to the noisy channel
+        """
+        bits_array = file_to_bits(self.input_file, little_endian=False) # Read file and get a bit array
         with open(self.input_file) as f:
             print(f.read())
-        values_array = self.bits_to_values(bits_array)
-        conv = self.convolution(values_array)
-        print(visualize_convolution(values_array, conv))
-        for bit in conv:
-            print(bit)
-        np.savetxt(self.output_file, values_array)
-        print(f'Total number of bits used : {len(values_array)}')
-    
-   
-def visualize_convolution(bit_vector, conv):
-    print("length comparaison : ")
-    print("conv = ", len(conv))
-    print("init = ", len(bit_vector))
-    print("ratio : ", len(conv) / len(bit_vector))
-    for i in range(len(bit_vector)):
-        if(i == 0):
-            print(f"b_0 = {bit_vector[i]}")
-        elif(i == 1):
-            print(f"b_0, b_1 = {(bit_vector[i - 1], bit_vector[i])}")
-        else :
-            print(f"bn-2, bn-1, bn = {(bit_vector[i - 2], bit_vector[i - 1], bit_vector[i])}")
-        print(f"(x2n, x2n+1) = {(conv[2*i], conv[2*i + 1])}")
+        plus_minus_array = bits_to_plus_minus_one(bits_array) # maps the bits {1 -> +1, 0 -> -1}
+        convolved = self.convolutional_encoding(plus_minus_array) # Compute the convolutional code
+        repeated = self.repetition_encoding(convolved, nb_repetitions=3) # Apply the repetition code to mitigate the erased index
+        np.savetxt(self.output_file, repeated)
+        print(f'Total number of bits used : {len(repeated)}')
+
+        
+def file_to_bits(filename, little_endian=True, verbose=True):
+    """
+    Convert input file to bit array
+    """
+    bits = []
+    with open(filename, 'rb') as f:
+        bytes = f.read()
+        if verbose:
+            print(f'Original file : {bytes}\n')
+        for b in bytes:
+            for i in range(8):
+                bits.append((b >> i) & 1 if little_endian else (b >> (7 - i)) & 1)
+        if verbose:
+            print(f'Bits array : {bits}\n')
+        return np.array(bits)
+            
+def bits_to_plus_minus_one(bits_array):
+    """
+    Convert bits to +1, -+1 representation
+    0 -> -1
+    1 -> +1
+    """
+    return list(map(lambda bit: (-1 if bit == 0 else 1), bits_array))
+
+def window(seq, size):
+    """
+    Creates a sliding window of size 'size' over the sequence 'seq'
+    """
+    it = iter(seq)
+    win = deque((next(it, None) for _ in range(size)), maxlen=size)
+    yield win
+    append = win.append
+    for e in it:
+        append(e)
+        yield win
+
